f490fba4274a2830bf3125f4e5182ad7c66d5673
Cwishart203 <93050589+Cwishart203@users.noreply.github.com>
2022-01-23 18:29:21 -0500

    page-template.js todo's

--- src/page-template.js
+++ src/page-template.js
@@ -22,8 +22,20 @@ const generateTeam = team => {
 
     // create the html for engineers
     const generateEngineer = engineer => {
-
         return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${engineer.getGithub()}</li>
+            </ul>
+        </div>
+    </div>
 
         `;
     };
@@ -33,6 +45,19 @@ const generateTeam = team => {
         // TODO: YOUR CODE HERE
         // fill out template literal in return
         return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${intern.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${intern.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>
 
         `;
     };
